4ba5b73cbf2371809a161ddc044e69fce462118a
neo849 <f.michi84.989@gmail.com>
2025-05-03 03:28:56 +0200

    Initial Commit

---
+++ Chakren/ChakrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChakrenApp.swift
+//  Chakren
+//
+//  Created by Michael Fleps on 03.05.25.
+//
+
+import SwiftUI
+
+@main
+struct ChakrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
