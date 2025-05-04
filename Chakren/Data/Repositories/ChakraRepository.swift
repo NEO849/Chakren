5480062f5ac8308e86bcb679c55d569059e3ca85
neo849 <f.michi84.989@gmail.com>
2025-05-04 07:36:35 +0200

    Repo das die json mittels Bundel.main fetcht

--- Chakren/Data/Repositories/ChakraRepository.swift
+++ Chakren/Data/Repositories/ChakraRepository.swift
@@ -8,17 +8,17 @@
 import Foundation
 
 /// Verwaltet die Chakra-Datenquelle (z. B. JSON-Datei im Bundle)
-class ChakraRepository {
+final class ChakraRepository {
     static func fetchChakras() throws -> [Chakra] {
-         // 1. JSON-Dateipfad im App-Bundle suchen
-         guard let url = Bundle.main.url(forResource: "chakras", withExtension: "json") else {
-             throw URLError(.fileDoesNotExist)
-         }
-         
-         // 2. Rohdaten aus der Datei lesen
-         let data = try Data(contentsOf: url)
-         
-         // 3. JSON-Daten in Chakra-Objekte umwandeln
-         return try JSONDecoder().decode([Chakra].self, from: data)
-     }
- }
+        // 1. JSON-Dateipfad im App-Bundle suchen
+        guard let url = Bundle.main.url(forResource: "chakras", withExtension: "json") else {
+            throw URLError(.fileDoesNotExist)
+        }
+        
+        // 2. Rohdaten aus der Datei lesen
+        let data = try Data(contentsOf: url)
+        
+        // 3. JSON-Daten in Chakra-Objekte umwandeln
+        return try JSONDecoder().decode([Chakra].self, from: data)
+    }
+}
